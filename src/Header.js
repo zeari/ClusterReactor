9596c4c976f5c63b9daad16a0b16a92e47d599b7
Elad Alfassa <ealfassa@redhat.com>
2018-06-12 14:55:20 +0300

    add cluster list, header

---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Masthead, MenuItem } from 'patternfly-react'
+import logo from './logo.svg';
+
+
+class Header extends Component {
+  render() {
+    return (
+        <Masthead
+          titleImg="?"
+          title="Patternfly React"
+          navToggle={true}
+        >
+          <Masthead.Collapse>
+            <Masthead.Dropdown id="app-help-dropdown" title={<span title="Help" className="pficon pficon-help" />}>
+              <MenuItem eventKey="1">Help</MenuItem>
+              <MenuItem eventKey="2">About</MenuItem>
+            </Masthead.Dropdown>
+            <Masthead.Dropdown
+              id="app-user-dropdown"
+              title={
+                <span>
+                  <span title="Help" className="pficon pficon-user" />
+                  <span className="dropdown-title">User name</span>
+                </span>
+              }
+            >
+              <MenuItem eventKey="1">User Preferences</MenuItem>
+              <MenuItem eventKey="2">Logout</MenuItem>
+            </Masthead.Dropdown>
+          </Masthead.Collapse>
+        </Masthead>
+    );
+  }
+}
+
+export { Header };
