20064c82e7de7f8b6e825135432cb4b77cf3a9f9
Elad Alfassa <ealfassa@redhat.com>
2018-06-13 12:31:04 +0300

    Create cluster button example (async, but no redux)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Header } from './Header.js';
 import { ClusterList } from './ClusterList.js';
+import { ListView, Icon, Button, Row, Col } from 'patternfly-react'
 import logo from './logo.svg';
 import "patternfly/dist/css/patternfly.css";
 import "patternfly/dist/css/patternfly-additions.css";
